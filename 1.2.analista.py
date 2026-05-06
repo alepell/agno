9165be43f2384299f6ca276ecee880202d8bcd3c
Alexandre Freire Pellegrino <alesirius@gmail.com>
2026-05-06 17:13:52 -0300

    feat: add instructions for response formatting and enable streaming in analista.py

--- 1.2.analista.py
+++ 1.2.analista.py
@@ -6,7 +6,8 @@ load_dotenv()
 
 agent  = Agent(
     model=Groq(id="llama-3.3-70b-versatile"),
-    tools=[YFinanceTools()]
+    tools=[YFinanceTools()],
+    instructions="Use tabelas para mostrar a informação final. Não inclua nenhum outro texto."
 )
 
-agent.print_response("Qual a cotação da Apple atual?")
+agent.print_response("Qual a cotação da Apple atual?", stream=True)
