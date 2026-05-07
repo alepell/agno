694c2ce10aa197f61add06f038379ea80c732259
Alexandre Freire Pellegrino <alesirius@gmail.com>
2026-05-06 23:07:26 -0300

    feat: enhance .gitignore for database files, integrate SqliteDb in agent configuration, and update dependencies in pyproject.toml and uv.lock

--- agno_playground.py
+++ agno_playground.py
@@ -3,6 +3,7 @@ from agno.tools.tavily import TavilyTools
 from agno.models.openai import OpenAIChat
 from fastapi.middleware.cors import CORSMiddleware
 from agno.os import AgentOS
+from agno.db.sqlite import SqliteDb
 
 # from agno.models.groq import Groq
 from dotenv import load_dotenv
@@ -27,6 +28,9 @@ agent = Agent(
     name="Agente do Tempo",
     model=OpenAIChat(id="gpt-5.4-mini"),
     tools=[TavilyTools(), celsius_to_fh],
+    add_history_to_context=True,
+    db=SqliteDb(db_file="agent.db"),
+    num_history_runs=3,
 )
 
 agent_os = AgentOS(agents=[agent])
